4a59ae6bc7b4f324a5673236e8e0fe23e24c9ea1
Sal Carrera <carrera.328@gmail.com>
2023-01-29 02:18:35 -0500

    GPT-3 middleware

---
+++ index.js
@@ -0,0 +1,67 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from 'express'
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const configuration = new Configuration({
+    apiKey: process.env.KEY,
+});
+
+
+app.post('/', async (req, res) => {
+    if (!configuration.apiKey) {
+        res.status(500).json({
+          error: {
+            message: "OpenAI API key not configured, please follow instructions in README.md",
+          }
+        });
+        return;
+      }
+
+      const lan = req.body.language || '';
+
+      try {
+
+        const openai = new OpenAIApi(configuration);
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompt(lan),
+            max_tokens: 1000
+            
+        });
+
+        res.send(completion.data.choices[0].text);
+        
+      } catch(error) {
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+          } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+              error: {
+                message: 'An error occurred during your request.',
+              }
+            });
+          } 
+      } 
+  })
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
+
+function generatePrompt(codeLanguage) {
+    return `write some ${codeLanguage} code`;
+}
+
+
+
